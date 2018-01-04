104262599b8e21c814eaeaa51675b0ad2263d65e
Juan Manuel Fresia <jfresia@medallia.com>
2018-01-04 13:33:58 -0300

    Restoring old structdump
    Signed-off-by: Juan Manuel Fresia <jfresia@medallia.com>

--- src/main/java/org/apache/aurora/scheduler/http/StructDump.java
+++ src/main/java/org/apache/aurora/scheduler/http/StructDump.java
@@ -22,10 +22,11 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
+import com.google.common.base.Joiner;
 import com.google.common.base.Optional;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
+import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
 import org.apache.aurora.scheduler.base.JobKeys;
 import org.apache.aurora.scheduler.storage.Storage;
 import org.apache.aurora.scheduler.storage.Storage.Work.Quiet;
@@ -33,6 +34,12 @@ import org.apache.aurora.scheduler.storage.entities.IJobConfiguration;
 import org.apache.aurora.scheduler.storage.entities.IJobKey;
 import org.apache.aurora.scheduler.storage.entities.IScheduledTask;
 import org.apache.thrift.TBase;
+import org.apache.thrift.TFieldIdEnum;
+import org.apache.thrift.meta_data.FieldMetaData;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import static java.util.Objects.requireNonNull;
 
@@ -99,18 +106,140 @@ public class StructDump extends JerseyTemplateServlet {
             .transform(IJobConfiguration::newBuilder));
   }
 
-  private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
 
   private Response dumpEntity(String id, Quiet<Optional<? extends TBase<?, ?>>> work) {
     return fillTemplate(template -> {
       template.setAttribute("id", id);
       Optional<? extends TBase<?, ?>> struct = storage.read(work);
       if (struct.isPresent()) {
-        template.setAttribute("structPretty", GSON.toJson(struct.get()));
+        template.setAttribute("structPretty", prettyPrint(struct.get()));
         template.setAttribute("exception", null);
       } else {
         template.setAttribute("exception", "Entity not found");
       }
     });
+  }
+
+
+  /**
+   * Pretty-prints a thrift object contents.
+   *
+   * @param t The thrift object to print.
+   * @return The pretty-printed version of the thrift object.
+   */
+  @SuppressWarnings("rawtypes")
+  private static String prettyPrint(TBase t) {
+    return t == null ? "null" : printTbase(t, 0);
+  }
+
+  /**
+   * Prints an object contained in a thrift message.
+   *
+   * @param o The object to print.
+   * @param depth The print nesting level.
+   * @return The pretty-printed version of the thrift field.
+   */
+  private static String printValue(Object o, int depth) {
+    if (o == null) {
+      return "null";
+    } else if (TBase.class.isAssignableFrom(o.getClass())) {
+      return "\n" + printTbase((TBase) o, depth + 1);
+    } else if (Map.class.isAssignableFrom(o.getClass())) {
+      return printMap((Map) o, depth + 1);
+    } else if (List.class.isAssignableFrom(o.getClass())) {
+      return printList((List) o, depth + 1);
+    } else if (Set.class.isAssignableFrom(o.getClass())) {
+      return printSet((Set) o, depth + 1);
+    } else if (String.class == o.getClass()) {
+      return '"' + o.toString() + '"';
+    } else {
+      return o.toString();
+    }
+  }
+
+  /**
+   * Prints a TBase.
+   *
+   * @param t The object to print.
+   * @param depth The print nesting level.
+   * @return The pretty-printed version of the TBase.
+   */
+  @SuppressWarnings("rawtypes")
+  private static String printTbase(TBase t, int depth) {
+    List<String> fields = Lists.newArrayList();
+    for (Map.Entry<? extends TFieldIdEnum, FieldMetaData> entry :
+            FieldMetaData.getStructMetaDataMap(t.getClass()).entrySet()) {
+      @SuppressWarnings("unchecked")
+      boolean fieldSet = t.isSet(entry.getKey());
+      String strValue;
+      if (fieldSet) {
+        @SuppressWarnings("unchecked")
+        Object value = t.getFieldValue(entry.getKey());
+        strValue = printValue(value, depth);
+      } else {
+        strValue = "not set";
+      }
+      fields.add(tabs(depth) + entry.getValue().fieldName + ": " + strValue);
+    }
+
+    return Joiner.on("\n").join(fields);
+  }
+
+  /**
+   * Prints a map in a style that is consistent with TBase pretty printing.
+   *
+   * @param map The map to print
+   * @param depth The print nesting level.
+   * @return The pretty-printed version of the map.
+   */
+  @SuppressWarnings("rawtypes")
+  private static String printMap(Map<?, ?> map, int depth) {
+    List<String> entries = Lists.newArrayList();
+    for (Map.Entry entry : map.entrySet()) {
+      entries.add(tabs(depth) + printValue(entry.getKey(), depth)
+              + " = " + printValue(entry.getValue(), depth));
+    }
+
+    return entries.isEmpty() ? "{}"
+            : String.format("{\n%s\n%s}", Joiner.on(",\n").join(entries), tabs(depth - 1));
+  }
+
+  /**
+   * Prints a list in a style that is consistent with TBase pretty printing.
+   *
+   * @param list The list to print
+   * @param depth The print nesting level.
+   * @return The pretty-printed version of the list
+   */
+  private static String printList(List<?> list, int depth) {
+    List<String> entries = Lists.newArrayList();
+    for (int i = 0; i < list.size(); i++) {
+      entries.add(
+              String.format("%sItem[%d] = %s", tabs(depth), i, printValue(list.get(i), depth)));
+    }
+
+    return entries.isEmpty() ? "[]"
+            : String.format("[\n%s\n%s]", Joiner.on(",\n").join(entries), tabs(depth - 1));
+  }
+  /**
+   * Prints a set in a style that is consistent with TBase pretty printing.
+   *
+   * @param set The set to print
+   * @param depth The print nesting level.
+   * @return The pretty-printed version of the set
+   */
+  private static String printSet(Set<?> set, int depth) {
+    List<String> entries = Lists.newArrayList();
+    for (Object item : set) {
+      entries.add(
+              String.format("%sItem = %s", tabs(depth), printValue(item, depth)));
+    }
+
+    return entries.isEmpty() ? "{}"
+            : String.format("{\n%s\n%s}", Joiner.on(",\n").join(entries), tabs(depth - 1));
+  }
+
+  private static String tabs(int n) {
+    return Strings.repeat("  ", n);
   }
 }
